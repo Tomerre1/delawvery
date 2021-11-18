dfae12f3f7c26cadf2214057d3af9592a919bcdc
Tomer <revahtomer@gmail.com>
2021-11-18 13:19:10 +0200

    add filter by lastname

--- src/cmps/Order/OrderList.jsx
+++ src/cmps/Order/OrderList.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import { OrderPreview } from './OrderPreview'
+import TextField from '@material-ui/core/TextField'
 
 export const OrderList = ({ orderToEdit, orders, onRemoveOrder, setEditOrder }) => {
+    const [filterByName, setFilterByName] = useState('')
 
     return (
         <div className="order-list flex column align-center">
@@ -9,7 +11,15 @@ export const OrderList = ({ orderToEdit, orders, onRemoveOrder, setEditOrder }) 
                 <h1>רשימת הזמנות</h1>
                 <p className="order-count flex space-between">מספר הזמנות: <span>{orders?.length}</span></p>
             </div>
-            {orders?.map((order) => <OrderPreview key={order._id} orderToEdit={orderToEdit} order={order} onRemoveOrder={onRemoveOrder} setEditOrder={setEditOrder} />)}
+            <TextField
+                variant="outlined"
+                placeholder=" הכנס שם משפחה לחיפוש"
+                inputProps={{ className: 'input', type: 'search' }}
+                FormHelperTextProps={{ style: { textAlign: 'right' } }}
+                value={filterByName}
+                onChange={(e) => setFilterByName(e.target.value)}
+            />
+            {orders?.filter(order => order.lastName.includes(filterByName)).map(order => <OrderPreview key={order._id} orderToEdit={orderToEdit} order={order} onRemoveOrder={onRemoveOrder} setEditOrder={setEditOrder} />)}
         </div>
     )
 }
