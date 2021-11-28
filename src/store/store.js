3b32ef3040b9eda31b66f4dd1d284f2a5fd67f8f
Tomer Revah <revahtomer@gmail.com>
2021-11-28 22:07:52 +0200

    ok

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,14 @@ import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { orderReducer } from './order.reducer'
 
+const rootReducer = combineReducers({
+    orderModule: orderReducer,
+})
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
-    orderReducer,
+    rootReducer,
     composeEnhancers(applyMiddleware(thunk))
 )
 
